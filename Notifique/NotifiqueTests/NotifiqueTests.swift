7135fb0aa5a2b03c57d9ba92f42e4139cfa77537
Christian Petersen <humanoidism@gmail.com>
2015-01-30 19:03:56 +0100

    Add tests

--- Notifique/NotifiqueTests/NotifiqueTests.swift
+++ Notifique/NotifiqueTests/NotifiqueTests.swift
@@ -2,35 +2,83 @@
 //  NotifiqueTests.swift
 //  NotifiqueTests
 //
-//  Created by iChrille on 30/01/15.
+//  Created by Christian Enevoldsen on 29/01/15.
 //  Copyright (c) 2015 Reversebox. All rights reserved.
 //
 
 import UIKit
 import XCTest
+import Notifique
 
 class NotifiqueTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  var count:Int = 0
+  var update: Int = 1
+  var update2: Int = 0
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    Notifique.on("target3").then(target: self, selector: "update:").then(target: self, selector: "update2:").then(target: self, selector: "update3:")
+    Notifique.on("target").then(target: self, selector: "update:")
+    Notifique.on("target2").then(target: self, selector: "update2:").then { (notificaition, object) -> Void in
+      self.update = 2
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    super.tearDown()
+  }
+  
+  func update(n: NSNotification) {
+    self.update = 1337
+  }
+  
+  func update2(n: NSNotification) {
+    self.update2 = 2
+  }
+  
+  func update3(n: NSNotification) {
+    self.update = 89
+  }
+  
+  func testThen() {
+    Notifique.on("test").then { (notificaition, object) -> Void in
+      self.count = 1
+      }.then { (notificaition, object) -> Void in
+        self.count = 2
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    NSNotificationCenter.defaultCenter().postNotificationName("test", object: nil)
+    XCTAssert(count == 2, "Failed")
+  }
+  
+  func testWith() {
+    var s = "with"
+    Notifique.on("test").with(object: s).then { (notificaition, object) -> Void in
+      XCTAssertEqual(s, (object as String), "")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    NSNotificationCenter.defaultCenter().postNotificationName("test", object: nil)
+  }
+  
+  
+  func testThenTarget() {
+    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+      NSNotificationCenter.defaultCenter().postNotificationName("target", object: self)
+      XCTAssert(self.update == 1337, "expected 1337 but got \(self.update)")
+    })
+  }
+  
+  func testThenThen() {
+    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+      NSNotificationCenter.defaultCenter().postNotificationName("target2", object: self)
+      XCTAssert(self.update == self.update2, "expected \(self.update) = \(self.update2)")
+    })
+  }
+  
+  func testThenThenTarget() {
+    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+      NSNotificationCenter.defaultCenter().postNotificationName("target3", object: self)
+      XCTAssert(self.update == 89, "expected \(self.update) = 89")
+    })
+  }
+  
 }
