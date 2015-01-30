95a05a09524b0a6ef240c6ca061f547223a23911
Christian Petersen <humanoidism@gmail.com>
2015-01-30 19:03:16 +0100

    Create new project

---
+++ Notifique/Notifique/Notifique.h
@@ -0,0 +1,19 @@
+//
+//  Notifique.h
+//  Notifique
+//
+//  Created by iChrille on 30/01/15.
+//  Copyright (c) 2015 Reversebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notifique.
+FOUNDATION_EXPORT double NotifiqueVersionNumber;
+
+//! Project version string for Notifique.
+FOUNDATION_EXPORT const unsigned char NotifiqueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notifique/PublicHeader.h>
+
+
